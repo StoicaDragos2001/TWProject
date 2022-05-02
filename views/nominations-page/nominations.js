3871352f951891cd9f5be4c09523d9ea3596b6b6
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2022-05-02 22:37:43 +0300

    Add files via upload

--- views/nominations-page/nominations.js
+++ views/nominations-page/nominations.js
@@ -1,3 +1,141 @@
+let data;
+let wonNomination = "all";
+let yearNomination = 2020;
+const url_nomination = `http://localhost:5000/nominations`;
+function statusNomination(response) {
+  if (response.status === 200) {
+    return Promise.resolve(response);
+  } else {
+    console.log(response);
+    return Promise.reject(new Error("An unexpected error occured"));
+  }
+}
+var index = 1;
+function initialization() {
+  var btnContainer = document.getElementById("myButtons");
+  var btns = btnContainer.getElementsByClassName("btn");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      index = this.id;
+      createCards(wonNomination, yearNomination);
+      this.className += " active";
+    });
+  }
+  var btnsSpecial = btnContainer.getElementsByClassName("special");
+  for (var i = 0; i < 2; i++) {
+    btnsSpecial[i].addEventListener("click", function () {
+      if (this.id == 0) {
+        if (index != 1) {
+          var current = document.getElementsByClassName("active");
+          current[0].className = current[0].className.replace(" active", "");
+          index--;
+          var indexArrow = document.getElementById(`${index}`);
+          console.log(indexArrow);
+          indexArrow.className += " active";
+          createCards(wonNomination, yearNomination);
+        }
+      } else if (this.id == 8) {
+        if (index != 7) {
+          var current = document.getElementsByClassName("active");
+          current[0].className = current[0].className.replace(" active", "");
+          index++;
+          var indexArrow = document.getElementById(`${index}`);
+          indexArrow.className += " active";
+          createCards(wonNomination, yearNomination);
+        }
+      }
+    });
+  }
+}
+
+var checkboxes = document.querySelectorAll(
+  "input[type=checkbox][name=filters]"
+);
+let enabledFilters = [];
+checkboxes.forEach(function (checkbox) {
+  checkbox.addEventListener("change", function () {
+    enabledFilters = Array.from(checkboxes)
+      .filter((i) => i.checked)
+      .map((i) => i.value);
+    if (enabledFilters.length == 2 || enabledFilters.length == 0)
+      wonNomination = "all";
+    else wonNomination = enabledFilters[0];
+    createCards(wonNomination, yearNomination);
+  });
+});
+var selectedYear = document.getElementById("year-select");
+selectedYear.addEventListener(
+  "change",
+  function () {
+    console.log(this.value);
+    yearNomination = this.value;
+    createCards(wonNomination, yearNomination);
+  },
+  false
+);
+async function getData() {
+  await fetch(url_nomination)
+    .then(statusNomination)
+    .then((res) => res.json())
+    .then((response) => {
+      data = response;
+    });
+}
+
+async function filter(wonNomination1, yearNomination1) {
+  var filteredData = [];
+
+  for (var i = 0; i < data.length; i++) {
+    var newYear = data[i].year.substring(0, 4);
+
+    if (
+      (data[i].won === wonNomination1 || wonNomination1 === "all") &&
+      newYear == yearNomination1
+    ) {
+      filteredData.push(data[i]);
+    }
+  }
+  return filteredData;
+}
+
+async function createCards(wonNomination, yearNomination) {
+  var oldCardsActors = document.getElementsByClassName("actor-card");
+  for (let j = 0; j < oldCardsActors.length; j++) {
+    oldCardsActors[j].remove();
+    j--;
+  }
+  var oldCardsMovie = document.getElementsByClassName("movie-card");
+  for (let k = 0; k < oldCardsMovie.length; k++) {
+    oldCardsMovie[k].remove();
+    k--;
+  }
+  var filteredData = await filter(wonNomination, yearNomination);
+  for (var i = 18 * (index - 1); i < 18 * index; i++) {
+    if (filteredData[i].full_name !== "") {
+      createActorCard(
+        filteredData[i].full_name,
+        filteredData[i].show,
+        filteredData[i].category,
+        filteredData[i].year,
+        filteredData[i].won
+      );
+    } else {
+      createMovieCard(
+        filteredData[i].show,
+        filteredData[i].category,
+        filteredData[i].year,
+        filteredData[i].won
+      );
+    }
+  }
+}
+async function getInformation() {
+  await getData();
+  await createCards(wonNomination, yearNomination);
+}
+
 var cards_parent = document.getElementById("cards");
 const trophy_icon = "fa fa-trophy fa-xs";
 // <div class="actor-card">
@@ -41,7 +179,7 @@ function createActorCard(name, movie, award, edition, recipient) {
   var trophy = document.createElement("i");
   trophy.setAttribute("class", trophy_icon);
   awarded.appendChild(trophy);
-  if (recipient == false) {
+  if (recipient === "False") {
     trophy.style.display = "none";
   }
 
@@ -112,7 +250,7 @@ function createMovieCard(movie, award, edition, recipient) {
   var trophy = document.createElement("i");
   trophy.setAttribute("class", trophy_icon);
   awarded.appendChild(trophy);
-  if (recipient == false) {
+  if (recipient == "False") {
     trophy.style.display = "none";
   }
 
@@ -128,25 +266,4 @@ function createMovieCard(movie, award, edition, recipient) {
   movie_edition.textContent = edition;
   movie_card_back.appendChild(movie_award);
   movie_card_back.appendChild(movie_edition);
-}
-
-function loadCards() {
-  let winner;
-  for (let i = 0; i < 15; i++) {
-    if (i % 4 == 0) winner = true;
-    else winner = false;
-    createActorCard(
-      "Scarlett Johansson",
-      "Marriage Story",
-      "Female actor in a leading role",
-      "2020 - 26th Annual",
-      winner
-    );
-    createMovieCard(
-      "Marriage Story",
-      "Best movie",
-      "2020 - 26th Annual",
-      winner
-    );
-  }
 }
