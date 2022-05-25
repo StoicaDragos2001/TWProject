b9dc6d253de2af70cecf348288cb5cc80b68c0d4
StoicaDragos2001 <79250297+StoicaDragos2001@users.noreply.github.com>
2022-05-25 21:26:39 +0300

    Add files via upload

--- views/movies-page/movies.js
+++ views/movies-page/movies.js
@@ -1,11 +1,121 @@
 var container = document.getElementById("filters");
 var filters = container.getElementsByClassName("genre");
+
+function statusMovie(response) {
+  if (response.status === 200) {
+    return Promise.resolve(response);
+  } else {
+    console.log(response);
+    return Promise.reject(new Error("An unexpected error occured"));
+  }
+}
+
+let index = 1;
+let genre = 0;
+function initialization() {
+  let btnContainer = document.getElementById("myButtons");
+  let btns = btnContainer.getElementsByClassName("btn");
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+      let current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      index = this.id;
+      if (genre == 0) getInformationMovies();
+      else getInformationMoviesGenre();
+      this.className += " active";
+    });
+  }
+  let btnsSpecial = btnContainer.getElementsByClassName("special");
+  for (let i = 0; i < 2; i++) {
+    btnsSpecial[i].addEventListener("click", function () {
+      if (this.id == 0) {
+        if (index != 1) {
+          let current = document.getElementsByClassName("active");
+          current[0].className = current[0].className.replace(" active", "");
+          index--;
+          let indexArrow = document.getElementById(`${index}`);
+          indexArrow.className += " active";
+          if (genre == 0) getInformationMovies();
+          else getInformationMoviesGenre();
+        }
+      } else if (this.id == 8) {
+        if (index != 7) {
+          let current = document.getElementsByClassName("active");
+          current[0].className = current[0].className.replace(" active", "");
+          index++;
+          let indexArrow = document.getElementById(`${index}`);
+          indexArrow.className += " active";
+          if (genre == 0) getInformationMovies();
+          else getInformationMoviesGenre();
+        }
+      }
+    });
+  }
+}
+
+let data;
+async function getDataMovies() {
+  console.log("smth");
+  let urlMoviesPage = `http://localhost:5000/movies/${index}`;
+  await fetch(urlMoviesPage)
+    .then(statusMovie)
+    .then((res) => res.json())
+    .then((response) => {
+      data = response;
+    });
+}
+
+async function getDataMoviesGenre() {
+  let urlMoviesPageGenre = `http://localhost:5000/movies/${genre}/${index}`;
+  await fetch(urlMoviesPageGenre)
+    .then(statusMovie)
+    .then((res) => res.json())
+    .then((response) => {
+      data = response;
+    });
+}
+
+var container = document.getElementById("filters");
+var filters = container.getElementsByClassName("genre");
 for (var i = 0; i < filters.length; i++) {
   filters[i].addEventListener("click", function () {
-    this.classList.toggle("active-genre");
+    var current = document.getElementsByClassName("active-genre");
+    current[0].className = current[0].className.replace(" active-genre", "");
+    genre = this.id;
+    this.className += " active-genre";
+    index = 1;
+    let currentPage = document.getElementsByClassName("active");
+    currentPage[0].className = current[0].className.replace(" active", "");
+    let indexArrow = document.getElementById(`${index}`);
+    indexArrow.className += " active";
+    if (genre == 0) getInformationMovies(index);
+    else getInformationMoviesGenre(index, genre);
   });
 }
 
+async function getInformationMovies() {
+  await getDataMovies();
+  let oldCards = document.getElementsByClassName("movie-card");
+  for (let j = 0; j < oldCards.length; j++) {
+    oldCards[j].remove();
+    j--;
+  }
+  for (let i = 0; i < data.length; i = i + 4) {
+    createMovieCard(data[i], data[i + 1], data[i + 2], data[i + 3]);
+  }
+}
+
+async function getInformationMoviesGenre() {
+  await getDataMoviesGenre();
+  let oldCards = document.getElementsByClassName("movie-card");
+  for (let j = 0; j < oldCards.length; j++) {
+    oldCards[j].remove();
+    j--;
+  }
+  for (let i = 0; i < data.length; i = i + 4) {
+    createMovieCard(data[i], data[i + 1], data[i + 2], data[i + 3]);
+  }
+}
 //   <div class="movie-card">
 //   <div class="movie-card-content">
 //     <div class="movie-card-front">
@@ -45,7 +155,8 @@ function createMovieCard(name, rating, overview_text, poster_url) {
   var movie_card_front = document.createElement("div");
   movie_card_front.classList.add("movie-card-front");
   movie_card_content.appendChild(movie_card_front);
-  movie_card_front.style.backgroundImage = "url('" + poster_url + "')";
+  movie_card_front.style.backgroundImage =
+    "url('" + "https://image.tmdb.org/t/p/original/" + poster_url + "')";
 
   var card_text = document.createElement("div");
   card_text.classList.add("card-text");
